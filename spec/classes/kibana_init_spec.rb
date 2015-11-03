d9bcc3a32094042cdfaa27dfc5e940cdf2cd1194
Cristian Falcas <falcas@optymyze.com>
2015-11-03 16:23:40 +0200

    fix spec tests

--- spec/classes/kibana_init_spec.rb
+++ spec/classes/kibana_init_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe 'kibana', :type => :class do
-  let(:facts) { { :concat_basedir => '/var/lib/puppet/concat' } }
-
-  it { should contain_class('kibana::package') }
-  it { should contain_class('kibana::config') }
-
+  it { should contain_class('kibana') }
 end
 
